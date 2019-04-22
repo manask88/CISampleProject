20b2fbba3e9c2996814771e38a987110c045a32a
Manuel Nakamurakare <mnakamurakare@stubhub.com>
2019-04-22 13:56:57 -0700

    more stuff

--- app/src/main/java/com/example/cisampleproject/MainActivity.kt
+++ app/src/main/java/com/example/cisampleproject/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.cisampleproject
 
 import android.support.v7.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
 import android.os.Bundle
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "11f655f9-356e-4083-bbe6-9496354605d9",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
